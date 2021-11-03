4fc0470acf6f7ea67b97e674e4bc8fdc6743e47d
Imi <imre.varga97@gmail.com>
2021-11-03 21:47:49 +0100

    create pokemons obj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([])
+  const [pokeApi, setpokeApi] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+
+  const getPokemons = async () => {
+    const res = await fetch(pokeApi)
+    const data = await res.json()
+
+
+
+    function getPokeObj(results) {
+      results.forEach(async (result) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${result.name}`)
+        const data = await res.json()
+        let currentPokemon = { name: data.forms[0].name, thumbnail: data.sprites.other.dream_world.front_default }
+        setPokemons(prev => [...prev, currentPokemon])
+
+      })
+      console.log(pokemons)
+    }
+
+    getPokeObj(data.results)
+
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemons.map((pokemon, i) => {
+        return (
+          <>
+            <div key={i}>{pokemon.name}</div>
+            <img src={pokemon.thumbnail} />
+          </>
+        )
+      })}
     </div>
   );
 }
